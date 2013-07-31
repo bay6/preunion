c09bdb2f818a99fd6674cabfdb45cae2daada6e5
soffolk <zlx.star@gmail.com>
2013-07-31 22:26:02 +0800

    Revert "Merge branch 'master' of github.com:bay6/preunion"
    This reverts commit dda5f6b42f3190966baccdfe995f60358c4e7216, reversing
    changes made to 16f92f18f1db68a22fb9f1ad1f8c2b7c406faa24.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Preunion::Application.routes.draw do
-<<<<<<< HEAD
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -16,23 +15,6 @@ Preunion::Application.routes.draw do
   #   resources :products
 
   # Example resource route with options:
-=======
-  # The priority is based upon order of creation:
-  # first created -> highest priority.
-
-  # Sample of regular route:
-  #   match 'products/:id' => 'catalog#view'
-  # Keep in mind you can assign values other than :controller and :action
-
-  # Sample of named route:
-  #   match 'products/:id/purchase' => 'catalog#purchase', :as => :purchase
-  # This route can be invoked with purchase_url(:id => product.id)
-
-  # Sample resource route (maps HTTP verbs to controller actions automatically):
-  #   resources :products
-
-  # Sample resource route with options:
->>>>>>> 6c34f5b556a30b73cc3c34b721c23494660503c9
   #   resources :products do
   #     member do
   #       get 'short'
@@ -44,17 +26,12 @@ Preunion::Application.routes.draw do
   #     end
   #   end
 
-<<<<<<< HEAD
   # Example resource route with sub-resources:
-=======
-  # Sample resource route with sub-resources:
->>>>>>> 6c34f5b556a30b73cc3c34b721c23494660503c9
   #   resources :products do
   #     resources :comments, :sales
   #     resource :seller
   #   end
 
-<<<<<<< HEAD
   # Example resource route with more complex sub-resources:
   #   resources :products do
   #     resources :comments
@@ -71,33 +48,9 @@ Preunion::Application.routes.draw do
   #   resources :photos, concerns: :toggleable
 
   # Example resource route within a namespace:
-=======
-  # Sample resource route with more complex sub-resources
-  #   resources :products do
-  #     resources :comments
-  #     resources :sales do
-  #       get 'recent', :on => :collection
-  #     end
-  #   end
-
-  # Sample resource route within a namespace:
->>>>>>> 6c34f5b556a30b73cc3c34b721c23494660503c9
   #   namespace :admin do
   #     # Directs /admin/products/* to Admin::ProductsController
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
-<<<<<<< HEAD
-=======
-
-  # You can have the root of your site routed with "root"
-  # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
-
-  # See how all your routes lay out with "rake routes"
-
-  # This is a legacy wild controller route that's not recommended for RESTful applications.
-  # Note: This route will make all actions in every controller accessible via GET requests.
-  # match ':controller(/:action(/:id))(.:format)'
->>>>>>> 6c34f5b556a30b73cc3c34b721c23494660503c9
 end
