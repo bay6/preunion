9236cf7b8ea93847e3355d55e690d63bc3d33663
soffolk <zlx.star@gmail.com>
2013-07-31 22:24:29 +0800

    Revert "delete old repo"
    This reverts commit 5d13549e21674c02bc0cabf3d1045f353ebb0236.

---
+++ Rakefile
@@ -0,0 +1,10 @@
+<<<<<<< HEAD
+=======
+#!/usr/bin/env rake
+>>>>>>> 6c34f5b556a30b73cc3c34b721c23494660503c9
+# Add your own tasks in files placed in lib/tasks ending in .rake,
+# for example lib/tasks/capistrano.rake, and they will automatically be available to Rake.
+
+require File.expand_path('../config/application', __FILE__)
+
+Preunion::Application.load_tasks
---
+++ config/routes.rb
@@ -0,0 +1,103 @@
+Preunion::Application.routes.draw do
+<<<<<<< HEAD
+  # The priority is based upon order of creation: first created -> highest priority.
+  # See how all your routes lay out with "rake routes".
+
+  # You can have the root of your site routed with "root"
+  # root 'welcome#index'
+
+  # Example of regular route:
+  #   get 'products/:id' => 'catalog#view'
+
+  # Example of named route that can be invoked with purchase_url(id: product.id)
+  #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
+
+  # Example resource route (maps HTTP verbs to controller actions automatically):
+  #   resources :products
+
+  # Example resource route with options:
+=======
+  # The priority is based upon order of creation:
+  # first created -> highest priority.
+
+  # Sample of regular route:
+  #   match 'products/:id' => 'catalog#view'
+  # Keep in mind you can assign values other than :controller and :action
+
+  # Sample of named route:
+  #   match 'products/:id/purchase' => 'catalog#purchase', :as => :purchase
+  # This route can be invoked with purchase_url(:id => product.id)
+
+  # Sample resource route (maps HTTP verbs to controller actions automatically):
+  #   resources :products
+
+  # Sample resource route with options:
+>>>>>>> 6c34f5b556a30b73cc3c34b721c23494660503c9
+  #   resources :products do
+  #     member do
+  #       get 'short'
+  #       post 'toggle'
+  #     end
+  #
+  #     collection do
+  #       get 'sold'
+  #     end
+  #   end
+
+<<<<<<< HEAD
+  # Example resource route with sub-resources:
+=======
+  # Sample resource route with sub-resources:
+>>>>>>> 6c34f5b556a30b73cc3c34b721c23494660503c9
+  #   resources :products do
+  #     resources :comments, :sales
+  #     resource :seller
+  #   end
+
+<<<<<<< HEAD
+  # Example resource route with more complex sub-resources:
+  #   resources :products do
+  #     resources :comments
+  #     resources :sales do
+  #       get 'recent', on: :collection
+  #     end
+  #   end
+  
+  # Example resource route with concerns:
+  #   concern :toggleable do
+  #     post 'toggle'
+  #   end
+  #   resources :posts, concerns: :toggleable
+  #   resources :photos, concerns: :toggleable
+
+  # Example resource route within a namespace:
+=======
+  # Sample resource route with more complex sub-resources
+  #   resources :products do
+  #     resources :comments
+  #     resources :sales do
+  #       get 'recent', :on => :collection
+  #     end
+  #   end
+
+  # Sample resource route within a namespace:
+>>>>>>> 6c34f5b556a30b73cc3c34b721c23494660503c9
+  #   namespace :admin do
+  #     # Directs /admin/products/* to Admin::ProductsController
+  #     # (app/controllers/admin/products_controller.rb)
+  #     resources :products
+  #   end
+<<<<<<< HEAD
+=======
+
+  # You can have the root of your site routed with "root"
+  # just remember to delete public/index.html.
+  # root :to => 'welcome#index'
+
+  # See how all your routes lay out with "rake routes"
+
+  # This is a legacy wild controller route that's not recommended for RESTful applications.
+  # Note: This route will make all actions in every controller accessible via GET requests.
+  # match ':controller(/:action(/:id))(.:format)'
+>>>>>>> 6c34f5b556a30b73cc3c34b721c23494660503c9
+end
