5d13549e21674c02bc0cabf3d1045f353ebb0236
soffolk <zlx.star@gmail.com>
2013-07-31 22:21:19 +0800

    delete old repo

--- app/controllers/application_controller.rb
+++
@@ -1,9 +0,0 @@
-class ApplicationController < ActionController::Base
-<<<<<<< HEAD
-  # Prevent CSRF attacks by raising an exception.
-  # For APIs, you may want to use :null_session instead.
-  protect_from_forgery with: :exception
-=======
-  protect_from_forgery
->>>>>>> 6c34f5b556a30b73cc3c34b721c23494660503c9
-end
