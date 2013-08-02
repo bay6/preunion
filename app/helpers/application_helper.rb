d2190ee53df5ec2c598187f1f37ca94f1bd1e25e
William Herry <WilliamHerryChina@Gmail.com>
2013-08-02 15:41:11 +0800

    mission page layout change

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
 end
+
+class String
+
+  def shorter(size)
+    if self.length > size
+      self[0..(size-1)] + "..."
+    else
+      self
+    end
+  end
+end
--- app/models/mission.rb
+++ app/models/mission.rb
@@ -3,6 +3,8 @@ class Mission < ActiveRecord::Base
   belongs_to :creator, class_name: "User"
   belongs_to :assigned_to, class_name: "User"
 
+  validates_presence_of :name, :description, :status
+
   STATUS = {
     0 => "新建",
     1 => "进行中",
