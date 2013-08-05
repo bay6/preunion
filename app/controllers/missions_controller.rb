912b9d255d9559660cc73965241aacc5f02d5afb
William Herry <WilliamHerryChina@Gmail.com>
2013-08-05 14:53:08 +0800

    latest event show in mission#index page

--- app/controllers/missions_controller.rb
+++ app/controllers/missions_controller.rb
@@ -2,6 +2,10 @@ class MissionsController < ApplicationController
   before_action :load_mission, only: [:create]
   load_and_authorize_resource
 
+  def index
+    @events = Event.order("created_at DESC").limit(15)
+  end
+
   def show
     @latest_missions = Mission.order("created_at DESC").limit(10)
   end
@@ -12,6 +16,7 @@ class MissionsController < ApplicationController
     @mission.status = 0
 
     if @mission.save
+      @mission.events.create(user: current_user, action: "create")
       redirect_to @mission, notice: t('common.create_success')
     else
       render action: 'new'
@@ -34,6 +39,7 @@ class MissionsController < ApplicationController
 
   def perform_action
     if @mission.perform_action(params[:action_name], current_user)
+      @mission.events.create(user: current_user, action: params[:action_name])
       redirect_to :back, notice: t("missions.#{params[:action_name]}_success")
     else
       redirect_to :back, notice: t("missions.#{params[:action_name]}_fail")
