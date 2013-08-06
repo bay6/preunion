2c727a7d4f94697d847bfab5b43bb4fac37132b0
soffolk <zlx.star@gmail.com>
2013-08-06 20:58:07 +0800

    revert heroku hack for migration

--- db/migrate/20130731194811_change_status_of_missions_to_integer.rb
+++ db/migrate/20130731194811_change_status_of_missions_to_integer.rb
@@ -1,14 +1,6 @@
 class ChangeStatusOfMissionsToInteger < ActiveRecord::Migration
   def up
-    if Rails.env.production?
-    connection.execute(%q{
-        alter table missions
-        alter column status
-        type integer using cast(status as integer)
-    })
-    else
-      change_column :missions, :status, :integer
-    end
+    change_column :missions, :status, :integer
   end
 
   def down
