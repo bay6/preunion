4eb46e34e44b13b57a780f2d9605b93114a04b0d
Ken Lu <ken@seeice.com>
2013-08-06 22:31:15 -0400

    updated omniauth

--- config/initializers/omniauth_github.rb
+++ config/initializers/omniauth_github.rb
@@ -1,7 +1,9 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   if Rails.env.production?
+    # callback is point to http://mission.rubyunion.com
+    provider :github, '1c8a8217f053c12d7e79', '405b009e1a191f6053b8217343e798c97654ebc5'
     # callback is point to http://thawing-basin-6896.herokuapp.com on heroku
-    provider :github, '857fff6a73997e787d5a', '277c4102c65e7598e190c089397a66aba18e4534'
+    #provider :github, '857fff6a73997e787d5a', '277c4102c65e7598e190c089397a66aba18e4534'
   elsif Rails.env.development?
     # callback is point to http://localhost:3000
     # if you want to auth in local, make sure your port is 3000
